699ac8682ea30b5bbdcbf3bdece9d23ac73bfc13
pierre3 <pierre3@outlook.com>
2018-05-04 20:32:46 +0900

    Initial

--- XamlRichMenuMaker/Properties/AssemblyInfo.cs
+++ XamlRichMenuMaker/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("XamlLineRichMenuEditor")]
+[assembly: AssemblyTitle("XamlRichMenuMaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XamlLineRichMenuEditor")]
+[assembly: AssemblyProduct("XamlRichMenuMaker")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

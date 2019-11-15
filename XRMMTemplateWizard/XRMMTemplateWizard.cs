ff03a8b3fd15c9c70c73c84f46ae8b446e8b5440
Hirotada Kobayashi <pierre3@outlook.com>
2019-11-15 19:47:52 +0900

    _

---
+++ XRMMTemplateWizard/XRMMTemplateWizard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace XRMMTemplateWizard
+{
+    public class XRMMTemplateWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            projectItem.Properties.Item("CopyToOutputDirectory").Value = 1;            
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath) => true;
+    }
+}
